7ac06254505e1bdf90eb023971ac9c542716e7a9
Parker Smart <psmart@spilledmilk.com>
2020-08-26 17:57:30 -0600

    Added cylinder.

---
+++ Physics/cylinder.js
@@ -0,0 +1,32 @@
+class Cylinder extends Element {
+
+    constructor (radius, height)
+    {
+        super();
+
+        this.collisionRadius = (height / 2 + radius) / 2;
+        this.height = height;
+        this.radius = radius;
+    }
+
+    changeHeight(height)
+    {
+        this.height = height;
+    }
+
+    changeRadius(radius)
+    {
+        this.radius = radius;
+    }
+
+    collided(element)
+    {
+        return (dist(this.location, element.location) < this.collisionRadius + element.collisionRadius)
+        || (dist(add(this.location, this.velocity), add(element.location, element.velocity)) < this.collisionRadius + element.collisionRadius);
+    }
+
+    drawElement() {
+        noStroke();     // TODO: Make this a property of the element.
+        cylinder(this.radius, this.height);
+    }
+}
--- Physics/sketch.js
+++ Physics/sketch.js
@@ -2,7 +2,7 @@
 
 let canvasSide = 500;
 let containmentScale = 2.0;
-let velocityScale = 2.5;
+let velocityScale = 1.5;
 
 let fishBowl = null;
 let balls = null;
@@ -22,7 +22,8 @@ function setup() {
     {
         //let element = new Ball(windowWidth / 200 * random(0.75, 1.0));
         //let element = new Box(createVector(windowWidth / 100 * random(0.75, 1.0), windowWidth / 100 * random(0.75, 1.0), windowWidth / 100 * random(0.75, 1.0)));
-        let element = new Cone(windowWidth / 100 * random(0.75, 1.0), windowWidth / 100 * random(0.75, 1.0) * 2);
+        //let element = new Cone(windowWidth / 100 * random(0.75, 1.0), windowWidth / 100 * random(0.75, 1.0) * 2);
+        let element = new Cylinder(windowWidth / 100 * random(0.75, 1.0), windowWidth / 100 * random(0.75, 1.0) * 2);
 
         let xVelocity = random(-1, 1) * velocityScale;
         let yVelocity = random(-1, 1) * velocityScale;
@@ -32,9 +33,10 @@ function setup() {
         element.changeColor(color(random(128,255), random(128,255), random(128,255)));
 
         let myFrameRate = 60; //frameRate();
-        let rotationPerSecond = 2 * PI / myFrameRate * random(0.1, 0.2);
+        let rotationPerSecond = 2 * PI / myFrameRate * random(0.1, 0.2) * (random() < 0.5 ? -1 : 1);
 
-        element.changeRotationalVelocity(createVector(rotationPerSecond, rotationPerSecond, 0));
+        //element.changeRotationalVelocity(createVector(rotationPerSecond, rotationPerSecond, 0));
+        element.changeRotationalVelocity(createVector(rotationPerSecond, 0, rotationPerSecond));
 
         scene.addElement(element);
     }
